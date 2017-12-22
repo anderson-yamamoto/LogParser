4c09a0dd0b5afda42d7521df702320132170aa85
Anderson Yamamoto <attogba@gmail.com>
2017-12-21 23:18:40 -0200

    Changed from Spring boot to Spring due to main class on java -cp issue

--- src/main/java/com/ef/Parser.java
+++ src/main/java/com/ef/Parser.java
@@ -3,35 +3,26 @@ package com.ef;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.ef.domain.LogPeriod;
 import com.ef.service.ParserService;
 import com.ef.util.CommandLineParameterParser;
 
-@SpringBootApplication
-public class Parser implements CommandLineRunner {
-
-	@Autowired
-	private ParserService service;
-
+public class Parser {
 
 	public static void main(String[] args) throws Exception {
-		SpringApplication.run(Parser.class, args);
-	}
-
-	@Override
-    public void run(String... args) throws Exception {
 		CommandLineParameterParser commandLine = new CommandLineParameterParser(args);
-		
-		LocalDateTime startDate = LocalDateTime.parse(commandLine.getStartDate(), DateTimeFormatter.ofPattern("yyyy-MM-dd.HH:mm:ss"));
-				
 
-    	//service.loadFileToDatabase(commandLine.getAccessLog());
-    	service.getIpsExceedingThreshold(startDate, LogPeriod.valueOf(commandLine.getDuration().toUpperCase()), commandLine.getThreshold()).forEach(ip -> System.out.println("Exceeded:" + ip));
-    }
+		LocalDateTime startDate = LocalDateTime.parse(commandLine.getStartDate(),
+				DateTimeFormatter.ofPattern("yyyy-MM-dd.HH:mm:ss"));
 
+		ApplicationContext ctx = new AnnotationConfigApplicationContext("com.ef");
+		ParserService service = ctx.getBean(ParserService.class);
+
+		// service.loadFileToDatabase(commandLine.getAccessLog());
+		service.getIpsExceedingThreshold(startDate, LogPeriod.valueOf(commandLine.getDuration().toUpperCase()),
+				commandLine.getThreshold()).forEach(ip -> System.out.println("Exceeded:" + ip));
+	}
 }
--- src/main/java/com/ef/ParserConfiguration.java
+++ src/main/java/com/ef/ParserConfiguration.java
@@ -2,17 +2,35 @@ package com.ef;
 
 import javax.sql.DataSource;
 
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
+@PropertySource(value = { "classpath:application.properties" })
 public class ParserConfiguration {
+    @Autowired
+    private Environment env;
+    
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(env.getRequiredProperty("logparser.datasource.driverClassName"));
+        dataSource.setUrl(env.getRequiredProperty("logparser.datasource.url"));
+        dataSource.setUsername(env.getRequiredProperty("logparser.datasource.username"));
+        dataSource.setPassword(env.getRequiredProperty("logparser.datasource.password"));
+        return dataSource;
+	}
 	
 	@Bean
-	@ConfigurationProperties(prefix="logparser.datasource")
-	public DataSource dataSource() {
-	    return DataSourceBuilder.create().build();
-	}
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        jdbcTemplate.setResultsMapCaseInsensitive(true);
+        return jdbcTemplate;
+    }
 }
+
